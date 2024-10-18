2ff8fcbfeea720e5abe2edacb7b281706b71ae95
SheerGhazi <sher.ghazi2192@gmail.com>
2024-10-19 00:11:42 +0500

    feat : Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,34 @@
 import React from 'react'
-import spt_logo from "../../assets/sptLogo.svg"
+import { FaSpotify } from "react-icons/fa";
+import { IoMdHome } from "react-icons/io";
+
+
 
 const Navbar = () => {
   return (
-    <div>
-      <img className='w-24 h-24' src={spt_logo} alt="" />
-      hello
+    <div className='p-3 h-[10vh]'>
+        <nav className='flex justify-between'>
+        <div className="sptIcon">
+      <FaSpotify className='size-8' />
+      </div>
+      <div className="searchBar flex justify-center items-center space-x-3">
+        <div className=''><IoMdHome className='size-8' /></div>
+      
+      <input className='bg-gray-600 rounded-2xl px-20 py-1' type="text" name="" id="" />
+      </div>
+      <div className="btns flex justify-center items-center space-x-3">
+        <div className="signInBtn text-gray-400 text-lg font-semibold hover:text-white ">
+            <a href="#">
+                <button>SignIn</button>
+            </a>
+        </div>
+        <div className="signUpBtn  " >
+        <a href="#">
+                <button className='bg-white text-black text-lg font-semibold rounded-2xl px-3'>SignUp</button>
+            </a>
+        </div>
+      </div>
+      </nav>
     </div>
   )
 }
