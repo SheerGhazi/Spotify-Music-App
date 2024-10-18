2dd8f4217c549cdf0eefca0e5ad11b4be499b43b
SheerGhazi <sher.ghazi2192@gmail.com>
2024-10-18 22:59:16 +0500

    feat : Vite

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import spt_logo from "../../assets/sptLogo.svg"
+
+const Navbar = () => {
+  return (
+    <div>
+      <img className='w-24 h-24' src={spt_logo} alt="" />
+      hello
+    </div>
+  )
+}
+
+export default Navbar
