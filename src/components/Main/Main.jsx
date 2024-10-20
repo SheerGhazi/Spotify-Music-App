e6cd9fa1268d4d741f3bf9e4adeda4a4d499a65c
SheerGhazi <sher.ghazi2192@gmail.com>
2024-10-20 22:50:12 +0500

    feat : Main.jsx

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,15 +1,22 @@
-import React from 'react'
+import React from "react";
 import { BiLibrary } from "react-icons/bi";
+import { FaPlus } from "react-icons/fa";
 
 const Main = () => {
   return (
     <div class="grid grid-cols-4 h-full m-4 gap-5">
-        <div class="col-span-1 bg-gray-900">
-        <BiLibrary />
+      <div class="col-span-1 bg-gray-900 flex justify-between">
+        <div className="lib flex">
+          <BiLibrary />
+          <h3 className="font-medium">Library</h3>
         </div>
-        <div class="col-span-3 bg-gray-900">75%</div>
+        <div className="plus-icon">
+          <FaPlus />
+        </div>
+      </div>
+      <div class="col-span-3 bg-gray-900">75%</div>
     </div>
-  )
-}
+  );
+};
 
-export default Main
+export default Main;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,36 +1,43 @@
-import React from 'react'
+import React from "react";
 import { FaSpotify } from "react-icons/fa";
 import { IoMdHome } from "react-icons/io";
 
-
-
 const Navbar = () => {
   return (
-    <div className='p-3 h-[10vh]'>
-        <nav className='flex justify-between'>
+    <div className="p-3 h-[10vh]">
+      <nav className="flex justify-between">
         <div className="sptIcon">
-      <FaSpotify className='size-8' />
-      </div>
-      <div className="searchBar flex justify-center items-center space-x-3">
-        <div className=''><IoMdHome className='size-8' /></div>
-      
-      <input className='bg-gray-600 rounded-2xl px-20 py-1' type="text" name="" id="" />
-      </div>
-      <div className="btns flex justify-center items-center space-x-3">
-        <div className="signInBtn text-gray-400 text-lg font-semibold hover:text-white ">
+          <FaSpotify className="size-8" />
+        </div>
+        <div className="searchBar flex justify-center items-center space-x-3">
+          <div className="">
+            <IoMdHome className="size-8" />
+          </div>
+
+          <input
+            className="bg-gray-600 rounded-2xl px-20 py-1"
+            type="text"
+            name=""
+            id=""
+          />
+        </div>
+        <div className="btns flex justify-center items-center space-x-3">
+          <div className="signInBtn text-gray-400 text-lg font-semibold hover:text-white ">
             <a href="#">
-                <button>SignIn</button>
+              <button>SignIn</button>
             </a>
-        </div>
-        <div className="signUpBtn  " >
-        <a href="#">
-                <button className='bg-white text-black text-lg font-semibold rounded-2xl px-3'>SignUp</button>
+          </div>
+          <div className="signUpBtn  ">
+            <a href="#">
+              <button className="bg-white text-black text-lg font-semibold rounded-2xl px-3">
+                SignUp
+              </button>
             </a>
+          </div>
         </div>
-      </div>
       </nav>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
